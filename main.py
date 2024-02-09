b388a693c31e61e29b5fc1c61fa4cf61442c0039
Higor Diego <higordiego@192.168.68.123>
2024-02-09 17:34:28 -0300

    add: information metadata image

--- main.py
+++ main.py
@@ -4,39 +4,53 @@ import sys
 import os
 
 from src.banner import banner, info
-from src.images import enhance_image
+from src.images import enhance_image, extract_info_image
 from src.videos import super_resolve_video
 
 def main():
     banner()
     info()  
-    
     parser = argparse.ArgumentParser(description='Melhoria da qualidade de imagem e vídeo.', 
                                      formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('--input', type=str, required=True, help='Caminho para a imagem de entrada')
-    parser.add_argument('--output', type=str, required=True, help='Caminho para o arquivo de saída')
-    parser.add_argument('--type', choices=['image', 'video'], required=True, help='Tipo de entrada (imagem ou vídeo)')
-    parser.add_argument('--model', type=str, required=True, help='Caminho para o arquivo do modelo. Ex: --model edsr/edsr_x2.pb')
+    parser.add_argument('--input', '-i', type=str, required=True, help='Caminho para a imagem de entrada')
+    parser.add_argument('--output', '-o', type=str, required=False, help='Caminho para o arquivo de saída')
+    parser.add_argument('--type', '-t', choices=['image', 'video'], required=True, help='Tipo de entrada (imagem ou vídeo)')
+    parser.add_argument('--show', '-s', action='store_true', help='Informa metadados da imagem')
+    parser.add_argument('--model', type=str, required=False, default="models/edsr/edsr_x2.pb", help='Caminho para o arquivo do modelo. Ex: --model models/edsr/edsr_x2.pb')
     args = parser.parse_args()
 
-    model_folder = os.path.dirname(args.model)
-    model_version = args.model.split('_')[1].split('.')[0] if len(args.model.split('_')) > 1 else None
+    model_folder = None
+    model_version = None
+    model_directory = None
+    if args.model: 
+      model_folder = os.path.dirname(args.model)
+      model_version = args.model.split('_')[1].split('.')[0] if len(args.model.split('_')) > 1 else None
 
-    pwd_directory = os.getcwd()
-    model_directory = os.path.join(pwd_directory, args.model)
+      pwd_directory = os.getcwd()
+      model_directory = os.path.join(pwd_directory, args.model)
 
     if args.type == 'image':
+      if args.show:
+        extract_info_image(args.input)
+      else:
         print('[x] - Aguarde um momento enquanto processamos sua imagem...')
         enhanced_image = enhance_image(args.input, model_directory, args.model)
         cv2.imwrite(args.output, enhanced_image)
+        
     elif args.type == 'video':
+        if args.model is None:
+          print('[o] - Parâmetro model é obrigatório para tipo de video')
+          sys.exit(1)
+        if args.output is None:
+          print('[o] - Parâmetro output é obrigatório para tipo de video')
+          sys.exit(1)
         print('[x] - Aguarde um momento enquanto processamos seu video...')
         super_resolve_video(args.input, args.output, model_directory, args.model)
 
 if __name__ == "__main__":
   try:
     main()
-    print('[x] - Processamento finalizado, espero ter ajuda-lo.')
+    print('[x] - Processo finalizado, espero ter ajuda-lo.')
     sys.exit(1)
   except KeyboardInterrupt:
     print("\nOperação interrompida pelo usuário.")
--- src/images/__init__.py
+++ src/images/__init__.py
@@ -1,7 +1,44 @@
 
 import cv2
+from PIL import Image
+from PIL.ExifTags import TAGS
+from pillow_heif import register_heif_opener
 
-from src.helpers import is_cuda_cv
+register_heif_opener()
+
+from src.helpers import is_cuda_cv, calculate_hash
+
+def extract_info_image(image_path):
+    print('[x] - Segue o resultado abaixo\n')
+    image = Image.open(image_path)
+    info_dict = {
+        "Filename": image.filename,
+        "Image Size": image.size,
+        "Image Height": image.height,
+        "Image Width": image.width,
+        "Image Format": image.format,
+        "Image Mode": image.mode,
+        "Image is Animated": getattr(image, "is_animated", False),
+        "Frames in Image": getattr(image, "n_frames", 1)
+    }
+
+    for label,value in info_dict.items():
+        print(f"{label:25}: {value}")
+
+    exifdata = image.getexif()
+
+    for tag_id in exifdata:
+        tag = TAGS.get(tag_id, tag_id)
+        data = exifdata.get(tag_id)
+        if isinstance(data, bytes):
+            data = data.decode()
+        print(f"{tag:25}: {data}")
+
+    file_hash = calculate_hash(image_path)
+    if file_hash:
+        print("hash                     :", file_hash)
+        print("\n")
+
 
 def enhance_image(image_path, model_path, name):
     sr = cv2.dnn_superres.DnnSuperResImpl_create()
