7bb7681eb329dd7d3840d24edbd237d7e6e3f2f6
Higor Diego <higordiego@192.168.68.123>
2024-02-10 16:34:54 -0300

    add: google maps pin geolocation url

--- src/images/__init__.py
+++ src/images/__init__.py
@@ -3,10 +3,49 @@ import cv2
 from PIL import Image
 from PIL.ExifTags import TAGS
 from pillow_heif import register_heif_opener
+from PIL.ExifTags import GPSTAGS
+import urllib.parse
 
 register_heif_opener()
 
 from src.helpers import is_cuda_cv, calculate_hash
+
+def get_exif(file_name):
+    image: Image.Image = Image.open(file_name)
+    return image.getexif()
+
+
+def get_geo(exif):
+    for key, value in TAGS.items():
+        if value == "GPSInfo":
+            break
+    gps_info = exif.get_ifd(key)
+    return {
+        GPSTAGS.get(key, key): value
+        for key, value in gps_info.items()
+    }
+
+def get_url_google_maps(latitude_decimal, longitude_decimal):
+    google_maps_url = f"https://www.google.com/maps/search/?api=1&query={latitude_decimal},{longitude_decimal}"
+    return urllib.parse.quote(google_maps_url, safe=':/?&=')
+
+def get_params_geo(geo):
+    refLat = geo['GPSLatitudeRef']
+    refLng = geo['GPSLongitudeRef']
+
+    lat = geo['GPSLatitude']
+    lng = geo['GPSLongitude']
+    
+    latitude_decimal = dms_to_decimal(lat, refLat)
+    longitude_decimal = dms_to_decimal(lng, refLng)
+    return get_url_google_maps(latitude_decimal, longitude_decimal)
+    
+def dms_to_decimal(dms, direction):
+    degrees, minutes, seconds = dms
+    decimal = float(degrees) + float(minutes)/60 + float(seconds)/3600
+    if direction in ['S', 'W']:  # S = sul, W = oeste
+        decimal *= -1
+    return decimal
 
 def extract_info_image(image_path):
     print('[x] - Segue o resultado abaixo\n')
@@ -22,8 +61,10 @@ def extract_info_image(image_path):
         "Frames in Image": getattr(image, "n_frames", 1)
     }
 
-    for label,value in info_dict.items():
-        print(f"{label:25}: {value}")
+    max_label_length = max(len(label) for label in info_dict.keys())
+
+    for label, value in info_dict.items():
+        print(f"{label:{max_label_length}}: {value}")
 
     exifdata = image.getexif()
 
@@ -32,13 +73,22 @@ def extract_info_image(image_path):
         data = exifdata.get(tag_id)
         if isinstance(data, bytes):
             data = data.decode()
-        print(f"{tag:25}: {data}")
+
+        print(f"{tag:{max_label_length}}: {data}")
+
+    geo = get_geo(exifdata)
+    
+    
+    for item in geo:
+        data = geo.get(item)
+        
+        print(f"{item:{max_label_length}}: {data}")
 
     file_hash = calculate_hash(image_path)
+    print(f"{'maps':{max_label_length}}: {get_params_geo(geo)}\n") 
     if file_hash:
-        print("hash                     :", file_hash)
-        print("\n")
-
+        print(f"{'hash':{max_label_length}}: {file_hash}\n")
+    
 
 def enhance_image(image_path, model_path, name):
     sr = cv2.dnn_superres.DnnSuperResImpl_create()
